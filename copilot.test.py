eb371e81dcd19c5a73a39e8130c4bc690beed2b5
sohail21400 <sohail21400@gmail.com>
2022-10-22 23:38:37 +0530

    creating backtesting program by ritvik -incomplete

--- copilot.test.py
+++ copilot.test.py
@@ -1,4 +1,7 @@
-# get 2 input and save it in a and b
-a, b = map(int, input().split())
-# print the sum of a and b
-print(a + b)
+# get 2 numbers a and b and save them in c
+def ab():
+    a = 1
+    b = 2
+    c = a + b
+    return c
+
